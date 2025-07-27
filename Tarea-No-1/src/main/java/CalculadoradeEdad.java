65f44ce7c5e4c175fa2ec319a517ab6499ff1f93
ASOP125 <aorozcop3@miumg.edu.gt>
2025-07-27 12:23:42 -0600

    Actualizada CalculadoradeEdad.java

--- Tarea-No-1/src/main/java/CalculadoradeEdad.java
+++ Tarea-No-1/src/main/java/CalculadoradeEdad.java
@@ -4,15 +4,15 @@ public class CalculadoradeEdad {
 
     public static void main(String[] args) {
         // Permite leer lo que el usuario escriba
-        Scanner scanner = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
 
         // Solicitar nombre al usuario
         System.out.print("Ingrese tu nombre: ");
-        String Nombre = scanner.nextLine(); //lee el texto donde se escribio el nombre
+        String Nombre = entrada.nextLine(); //lee el texto donde se escribio el nombre
 
         // Solicitar el año de nacimiento del usuario
         System.out.print("Ingresa tu año de nacimiento: ");
-        int AñoNacimiento = scanner.nextInt(); // lee un numero entero
+        int AñoNacimiento = entrada.nextInt(); // lee un numero entero
 
         // Calcular la edad aproximada utilizando el año actual (2025)
         int Edad = 2025 - AñoNacimiento;
@@ -21,7 +21,7 @@ public class CalculadoradeEdad {
         System.out.println("Hola "+ Nombre + ", tu edad aproximada es: " + Edad + " años. " );
 
         // Cerrar el scanner
-        scanner.close();
+        entrada.close();
 
     }
 }
